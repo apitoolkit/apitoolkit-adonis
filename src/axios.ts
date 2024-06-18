5314ff143fde740ee14c916740f49e222a205337
dawkaka <yousiph77@gmail.com>
2024-06-18 13:56:55 +0000

    clean up axios monitoring

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,14 @@
 import { HttpContext } from '@adonisjs/core/http'
-import { AxiosError, AxiosInstance, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
+import {
+  AxiosError,
+  AxiosInstance,
+  AxiosResponse,
+  AxiosStatic,
+  InternalAxiosRequestConfig,
+} from 'axios'
 import { buildPayload } from 'apitoolkit-js'
-import { ATError } from './types.js'
+import { APIToolkitConfig, ATError } from './types.js'
+import APIToolkitMiddleware from './middleware/apitoolkit_middleware.js'
 
 declare module 'axios' {
   export interface InternalAxiosRequestConfig {
@@ -18,45 +25,42 @@ export const onRequestError = (error: AxiosError): Promise<AxiosError> => {
   return Promise.reject(error)
 }
 
-export const onResponse =
-  (
-    urlWildcard: string | undefined,
-    redactHeaderLists: string[],
-    redactRequestBody: string[],
-    redactResponseBody: string[]
-  ) =>
-  (response: AxiosResponse): AxiosResponse => {
-    const ctx = HttpContext.get()
-    if (!ctx) {
-      console.log(
-        'APIToolkit: Context not found, make sure to enable asyncLocalStorage in your project'
-      )
-      return response
-    }
-    if (!ctx.apitoolkitData) {
-      console.log(
-        'APIToolkit: data not found, make sure to set disable to false in apitoolkitConfig'
-      )
-      return response
-    }
-    const req = response.config
-    const res = response
+function processResponse(
+  response: AxiosResponse | AxiosError,
+  urlWildcard: string | undefined,
+  redactHeaderLists: string[],
+  redactRequestBody: string[],
+  redactResponseBody: string[],
+  notWebContext: boolean,
+  isGlobal: boolean,
+  client: any
+) {
+  let req: any = response.config
+  let res: AxiosResponse | AxiosError | undefined = response
+  if (response instanceof Error) {
+    res = response.response
+    req = response.request
+  } else {
+    res = response
+  }
+  const ctx = HttpContext.get()
+  const reqBody = typeof req?.data === 'string' ? req.data : JSON.stringify(req?.data || {})
+  const respBody = typeof res?.data === 'string' ? res?.data : JSON.stringify(res?.data || {})
+  const errors: ATError[] = []
+  const { path: urlPath, rawUrl, queryParams } = getPathAndQueryParamsFromURL(req.url ?? '')
 
-    const reqBody = typeof req?.data === 'string' ? req.data : JSON.stringify(req?.data || {})
-    const respBody =
-      typeof req?.data === 'string' ? (res?.data as string) : JSON.stringify(res?.data || {})
-
-    const project_id = ctx.apitoolkitData.project_id
-    const ATClient = ctx.apitoolkitData.client
-    const ATConfig = ctx.apitoolkitData.config
-    const parent_id = ctx.apitoolkitData.msg_id
+  if (notWebContext && client) {
+    const config = client.getConfig()
+    const project_id = config.project_id
+    const ATConfig: APIToolkitConfig = config.config
+    const parent_id: any = undefined
 
     const errors: ATError[] = []
-    const { path: urlPath, rawUrl, queryParams } = getPathAndQueryParamsFromURL(req.url ?? '')
+
     const payload = buildPayload({
-      start_time: response.config.meta.startTime,
+      start_time: response.config?.meta.startTime || process.hrtime.bigint(),
       requestHeaders: req.headers,
-      responseHeaders: res.headers,
+      responseHeaders: res?.headers || {},
       reqParams: req.params,
       reqQuery: queryParams,
       reqBody,
@@ -75,11 +79,98 @@ export const onResponse =
       url_path: urlWildcard || urlPath,
       host: req.baseURL || '',
       method: req.method || '',
-      status_code: res.status,
+      status_code: res?.status || 404,
     })
 
-    ATClient.publishMessage(payload)
-    return response
+    client.publishMessage(payload)
+  } else {
+    let config
+    let ATClient
+    let project_id
+    let ATConfig: APIToolkitConfig | undefined
+    let parent_id
+
+    if (isGlobal) {
+      config = client.getConfig()
+      ATClient = client
+      project_id = config.project_id
+      ATConfig = config.config
+      parent_id = ctx?.apitoolkitData.msg_id || undefined
+    } else if (ctx) {
+      project_id = ctx.apitoolkitData.project_id
+      ATClient = ctx.apitoolkitData.client
+      ATConfig = ctx.apitoolkitData.config
+      parent_id = ctx.apitoolkitData.msg_id
+    }
+    if (ATConfig) {
+      const payload = buildPayload({
+        start_time: response.config?.meta.startTime || process.hrtime.bigint(),
+        requestHeaders: req.headers,
+        responseHeaders: res?.headers || {},
+        reqParams: req.params,
+        reqQuery: queryParams,
+        reqBody,
+        respBody,
+        sdk_type: 'JsAxiosOutgoing' as any,
+        redactRequestBody: redactRequestBody,
+        redactResponseBody: redactResponseBody,
+        redactHeaderLists: redactHeaderLists,
+        project_id,
+        service_version: ATConfig.serviceVersion,
+        errors,
+        tags: ATConfig.tags ?? [],
+        parent_id,
+        raw_url: rawUrl,
+        msg_id: undefined as any,
+        url_path: urlWildcard || urlPath,
+        host: getHostFromUrl(req.baseURL),
+        method: req.method || 'GET',
+        status_code: res?.status || 404,
+      })
+      ATClient.publishMessage(payload)
+    }
+  }
+}
+
+export const onResponse =
+  (
+    urlWildcard: string | undefined,
+    redactHeaderLists: string[],
+    redactRequestBody: string[],
+    redactResponseBody: string[],
+    notWebContext: boolean,
+    isGlobal: boolean,
+    client: any
+  ) =>
+  (response: AxiosResponse): AxiosResponse => {
+    const ctx = HttpContext.get()
+    if (!ctx && !notWebContext && !isGlobal) {
+      console.log(
+        'APIToolkit: Context not found, make sure to enable asyncLocalStorage in your project'
+      )
+      return response
+    }
+    if (ctx && !isGlobal && !ctx.apitoolkitData) {
+      console.log(
+        'APIToolkit: data not found, make sure to set disable to false in apitoolkitConfig'
+      )
+      return response
+    }
+    try {
+      processResponse(
+        response,
+        urlWildcard,
+        redactHeaderLists,
+        redactRequestBody,
+        redactResponseBody,
+        notWebContext,
+        isGlobal,
+        client
+      )
+      return response
+    } catch (error) {
+      return response
+    }
   }
 
 export const onResponseError =
@@ -87,79 +178,134 @@ export const onResponseError =
     urlWildcard: string | undefined,
     redactHeaderLists: string[],
     redactRequestBody: string[],
-    redactResponseBody: string[]
+    redactResponseBody: string[],
+    notWebContext: boolean,
+    isGlobal: boolean,
+    client: any
   ) =>
   (error: AxiosError): Promise<AxiosError> => {
     const ctx = HttpContext.get()
-    if (!ctx) {
+    if (!ctx && !notWebContext && !isGlobal) {
       console.log(
         'APIToolkit: Context not found, make sure to enable asyncLocalStorage in your project'
       )
       return Promise.reject(error)
     }
-    if (!ctx.apitoolkitData) {
+    if (ctx && !isGlobal && !ctx.apitoolkitData) {
       console.log(
         'APIToolkit: data not found, make sure to set disable to false in apitoolkitConfig'
       )
       return Promise.reject(error)
     }
 
-    const req = error.config
-    const res = error.response
-
-    const reqBody = typeof req?.data === 'string' ? req.data : JSON.stringify(req?.data || {})
-    const respBody =
-      typeof req?.data === 'string' ? (res?.data as string) : JSON.stringify(res?.data || {})
-
-    const project_id = ctx.apitoolkitData.project_id
-    const ATClient = ctx.apitoolkitData.client
-    const ATConfig = ctx.apitoolkitData.config
-    const parent_id = ctx.apitoolkitData.msg_id
-
-    const errors: ATError[] = []
-
-    const { path: urlPath, rawUrl, queryParams } = getPathAndQueryParamsFromURL(req?.url ?? '')
-    const payload = buildPayload({
-      start_time: error.config?.meta.startTime ?? process.hrtime.bigint(),
-      requestHeaders: req?.headers || {},
-      responseHeaders: res?.headers || {},
-      reqParams: req?.params || {},
-      reqQuery: queryParams,
-      reqBody,
-      respBody,
-      sdk_type: 'JSAxiosOutgoing' as any,
-      redactRequestBody: redactRequestBody,
-      redactResponseBody: redactResponseBody,
-      redactHeaderLists: redactHeaderLists,
-      project_id,
-      service_version: ATConfig.serviceVersion,
-      errors,
-      tags: ATConfig.tags ?? [],
-      parent_id,
-      raw_url: rawUrl,
-      msg_id: undefined as any,
-      url_path: urlWildcard || urlPath,
-      host: req?.baseURL || '',
-      method: req?.method || '',
-      status_code: res?.status || 404,
-    })
-
-    ATClient.publishMessage(payload)
-
-    return Promise.reject(error)
+    try {
+      processResponse(
+        error,
+        urlWildcard,
+        redactHeaderLists,
+        redactRequestBody,
+        redactResponseBody,
+        notWebContext,
+        isGlobal,
+        client
+      )
+      return Promise.reject(error)
+    } catch (error) {
+      return Promise.reject(error)
+    }
   }
 
 export function observeAxios(
-  axiosInstance: AxiosInstance,
+  axiosStatic: AxiosStatic,
   urlWildcard: string | undefined = undefined,
   redactHeaders: string[] = [],
   redactRequestBody: string[] = [],
   redactResponseBody: string[] = []
 ): AxiosInstance {
+  const axiosInstance = axiosStatic.create()
   axiosInstance.interceptors.request.use(onRequest, onRequestError)
   axiosInstance.interceptors.response.use(
-    onResponse(urlWildcard, redactHeaders, redactRequestBody, redactResponseBody),
-    onResponseError(urlWildcard, redactHeaders, redactRequestBody, redactResponseBody)
+    onResponse(
+      urlWildcard,
+      redactHeaders,
+      redactRequestBody,
+      redactResponseBody,
+      false,
+      false,
+      undefined
+    ),
+    onResponseError(
+      urlWildcard,
+      redactHeaders,
+      redactRequestBody,
+      redactResponseBody,
+      false,
+      false,
+      undefined
+    )
+  )
+  return axiosInstance
+}
+export function observeAxiosNotWebContext(
+  axiosStatic: AxiosStatic,
+  urlWildcard: string | undefined = undefined,
+  redactHeaders: string[] = [],
+  redactRequestBody: string[] = [],
+  redactResponseBody: string[] = [],
+  client: any
+): AxiosInstance {
+  const axiosInstance = axiosStatic.create()
+  axiosInstance.interceptors.request.use(onRequest, onRequestError)
+  axiosInstance.interceptors.response.use(
+    onResponse(
+      urlWildcard,
+      redactHeaders,
+      redactRequestBody,
+      redactResponseBody,
+      true,
+      false,
+      client
+    ),
+    onResponseError(
+      urlWildcard,
+      redactHeaders,
+      redactRequestBody,
+      redactResponseBody,
+      true,
+      false,
+      client
+    )
+  )
+  return axiosInstance
+}
+export function observeAxiosGlobal(
+  axiosInstance: AxiosInstance,
+  urlWildcard: string | undefined = undefined,
+  redactHeaders: string[] = [],
+  redactRequestBody: string[] = [],
+  redactResponseBody: string[] = [],
+  client: APIToolkitMiddleware
+): AxiosInstance {
+  axiosInstance.interceptors.request.use(onRequest, onRequestError)
+  axiosInstance.interceptors.response.use(
+    onResponse(
+      urlWildcard,
+      redactHeaders,
+      redactRequestBody,
+      redactResponseBody,
+      false,
+      true,
+      client
+    ),
+    onResponseError(
+      urlWildcard,
+      redactHeaders,
+      redactRequestBody,
+      redactResponseBody,
+      false,
+      true,
+      client
+    )
   )
   return axiosInstance
 }
@@ -177,5 +323,14 @@ function getPathAndQueryParamsFromURL(url: string) {
     return { path, queryParams, rawUrl: path + queryParamsString }
   } catch (error) {
     return { path: '', queryParams: {}, rawUrl: '' }
+  }
+}
+
+function getHostFromUrl(url: string) {
+  try {
+    const parsedUrl = new URL(url)
+    return parsedUrl.host
+  } catch (error) {
+    return ''
   }
 }

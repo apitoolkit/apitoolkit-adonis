74d65aa1a2c83743c21d92677c9aa0513f1a5009
dawkaka <yousiph77@gmail.com>
2024-06-18 16:46:06 +0000

    new exports

--- index.ts
+++ index.ts
@@ -3,4 +3,5 @@ export { observeAxios } from './src/axios.js'
 export { reportError } from './src/reportErrors.js'
 export { defineConfig } from './src/define_config.js'
 import APIToolkitMiddleware from './src/middleware/apitoolkit_middleware.js'
-export default APIToolkitMiddleware
+const APIToolkit = APIToolkitMiddleware
+export default APIToolkit
